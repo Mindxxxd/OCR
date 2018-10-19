5da0b811f6eb51af7dead91b33975d6d64c36961
Mindxxxd <5ci3nc3Mind@googlemail.com>
2018-10-19 18:12:31 +0200

    getting Mainactivity to work

--- app/src/main/java/com/example/surfacepro3/ocr/MainActivity.java
+++ app/src/main/java/com/example/surfacepro3/ocr/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.surfacepro3.ocr;
 
+import android.icu.lang.UCharacter;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.widget.TextView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,16 +18,20 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private DrawerLayout mDrLay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        mDrLay = findViewById(R.id.LayoutMain);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarMain);
         setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabMain);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,6 +43,20 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(R.string.helloWorld);
+
+        NavigationView navigationView = findViewById(R.id.NavBar);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mDrLay.closeDrawers();
+
+                        //TODO set menu action (Swap Fragments)
+                        return true;
+                    }
+                }
+        );
     }
 
     @Override
@@ -51,6 +75,9 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        } else if (id == android.R.id.home) {
+            mDrLay.openDrawer(GravityCompat.START);
             return true;
         }
 
